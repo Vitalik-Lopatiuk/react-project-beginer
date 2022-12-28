f4c17bd03a7b305827daa1ec60846e01e9abe756
Vitalik-Lopatiuk <workingprogram11@gmail.com>
2022-12-28 13:43:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './index.scss';
 
 function App() {
   const [open , setOpen] = useState(false)
-
+ 
   const showBtn = () =>{
     setOpen(true)
   }
@@ -12,19 +12,21 @@ function App() {
   const closeBtn = () =>{
     setOpen(false)
   }
+
+
   
   return (
     <div className="App">
       <button onClick={showBtn} className="open-modal-btn">✨ Открыть окно</button>
-      {open &&  <div className="overlay">
+     <div className={`overlay animated ${open ? 'show ' : ''}`}>
         <div className="modal">
-          <svg  height="200" viewBox="0 0 200 200" width="200" onClick={closeBtn}>
+          <svg className='' height="200" viewBox="0 0 200 200" width="200" onClick={closeBtn}>
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="people" />
         </div>
-      </div> }
+      </div> 
     </div>
   ); 
 }
