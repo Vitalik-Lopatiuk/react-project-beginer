ec2e831392805edbfc0803cda323129e28f8c72e
Vitalik_Lopatiuk <workingprogram11@gmail.com>
2023-01-12 01:19:17 +0200

    add adaptive

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -9,9 +9,9 @@ function Game({step , nextQuestion , question}) {
           <div style={{ width: `${editWidth}%` }} className="progress__inner"></div>
         </div>
         <h1>{question.title}</h1>
-        <ul>
+        <ul className='question-ul'>
           {
-            question.variants.map((text , i) =>  <li key={text} onClick={ () => nextQuestion(i)}>{text}</li> ) 
+            question.variants.map((text , i) =>  <li className='question-li' key={text} onClick={ () => nextQuestion(i)}>{text}</li> ) 
           }
        
         </ul>
